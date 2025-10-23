4d1b958c4c5afa8c299cf30af659f177ba9ec42f
Raul Oliveira <raulodl@gmail.com>
2025-10-23 11:20:31 -0300

    fix: custom hero type

--- src/components/overrides/Hero.tsx
+++ src/components/overrides/Hero.tsx
@@ -9,6 +9,7 @@ const override: SectionOverride = {
       Component: () => (
         <CustomHero
           image="https://ninjasomfaststore.vtexassets.com/assets/vtex.file-manager-graphql/images/98eefc48-78aa-4643-9c62-c8cc0976fcf4___a4091e97ce8e728d4622267f0db1442a.png"
+          imageMobile="https://ninjasomfaststore.vtexassets.com/assets/vtex.file-manager-graphql/images/98eefc48-78aa-4643-9c62-c8cc0976fcf4___a4091e97ce8e728d4622267f0db1442a.png"
           link="/microfone-vocal-dinamico-cardioide-sm-58-lc---shure-11460/p"
         />
       ),
